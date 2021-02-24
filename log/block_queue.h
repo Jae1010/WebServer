97f27ce7fa98a58cc27bfe350d932d3d4d2a1fdc
Jae1010 <jae335181140@gmail.com>
2021-02-23 23:40:22 -0800

    p

---
+++ log/block_queue.h
@@ -0,0 +1,165 @@
+/*************************************************************************
+	> File Name: block_queue.h
+	> Author: 
+	> Mail: 
+ ************************************************************************/
+
+#ifndef _BLOCK_QUEUE_H
+#define _BLOCK_QUEUE_H
+
+#include <iostream>
+#include <stdio.h>
+#include <pthread.h>
+#include "../lock/locker.h"
+
+using namespace std;
+
+template<class T>
+class block_queue
+{
+public:
+    block_queue(int max_size = 1000)
+    {
+        if(max_size <= 0)
+        {
+            exit(-1);
+        }
+
+        m_max_size = max_size;
+        m_array =new T[max_size];
+        m_size = 0;
+        m_front = -1;
+        m_back = -1;
+    }
+    void clear()
+    {
+        m_mutex.lock();
+        m_size = 0;
+        m_front = -1;
+        m_back = -1;
+        m_mutex.unclock();
+    }
+    ~block_queue()
+    {
+        m_mutex.lock();
+        if(m_array != NULL)
+        {
+            delete[] m_array;
+        }
+        m_mutex.unclock();
+    }
+    //判断队列是否满了
+    bool full()
+    {
+        m_mutex.lock();
+        if(m_size >= m_max_size)
+        {
+            m_mutex.unclock();
+            return true;
+        }
+        m_mutex.unclock();
+        return false;
+    }
+    //判断队列是否为空
+    bool empty()
+    {
+        m_mutex.lock();
+        if( 0 == m_size )
+        {
+            m_mutex.unclock();
+            return true;
+        }
+        m_mutex.unclock();
+        return false;
+    }
+    //返回队首元素
+    bool front(T &value)
+    {
+        m_mutex.lock();
+        if( 0 == m_size )
+        {
+            m_mutex.unclock();
+            return false;
+        }
+        value = m_array[m_front];
+        m_mutex.unclock();
+        return true;
+    }
+     //返回队尾元素
+    bool back()
+    {
+        m_mutex.lock();
+        if(0 == m_size)
+        {
+            m_mutex.unclock();
+            return false;
+        }
+        value = m_array[back];
+        m_mutex.unclock();
+        return true;
+    }
+    int size()
+    {
+        int tmp = 0;
+        m_mutex.lock();
+        tmp = m_size;
+        m_mutex.unclock();
+        return tmp;
+    }
+    int max_size()
+    {
+        int tmp = 0;
+        m_mutex.lock();
+        tmp = m_max_size;
+        m_mutex.unclock();
+        return tmp;
+    }
+    bool push(const T &item)
+    {
+        m_mutex.lock();
+        if(m_size >= m_max_size)
+        {
+            m_cond.broadcast();
+            m_mutex.unclock();
+            return false;
+        }
+        m_back = (m_back + 1) % m_max_size;
+        m_array[m_back] = item;
+
+        m_size++;
+
+        m_cond.broadcast();
+        m_mutex.unclock();
+        return true;
+    }
+    bool pop(T &item)
+    {
+        m_mutex.lock();
+        while(m_size <= 0)
+        {
+            if(!m_cond.wait(m_mutex.get()))
+            {
+                m_mutex.unclock();
+                return false;
+            }
+        }
+
+        m_front = (m_front + 1 ) % m_max_size;
+        item = m_array[front];
+        m_size --;
+        m_mutex.unclock();
+        return true;
+    }
+
+private:
+    locker m_mutex;
+    cond m_cond;
+
+    T *m_array;
+    int m_size;
+    int m_max_size;
+    int m_front;
+    int m_back;
+};
+
+#endif
